294a229fe69031bda7cf4d86a3ea22425711488b
Gyan Bharti <gyanbhartip@gmail.com>
2024-01-16 02:23:11 +0530

    feat: added profile api serializer, viewset and url

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
-from profiles_api import serializers
+from profiles_api import serializers, models
 
 
 class HelloApiView(APIView):
@@ -101,3 +101,10 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({"http_method": "DELETE"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+
+    serializer_class = serializers.UserProfilesSerializer
+    queryset = models.UserProfile.objects.all()
